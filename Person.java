b87205ed1191eb0bc203f8a010d9f52c8377a3bb
Muhammad faris shafwan <73006228+Faris072@users.noreply.github.com>
2024-10-03 18:38:21 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -15,7 +15,7 @@ class Person {
         fungsi displayInfo tidak pernah di gunakan baik di class main ataupun selain main.
         method displayInfo ini hanya untuk override pada class turunannya saja.
         pembentukan object person sendiri tidak pernah dilakukan di main, displayInfo hanya untuk override. 
-        seharusnya tidak perlu dilakukan
+        seharusnya dihapus saja
     */
     public void displayInfo() {
         System.out.println("Name: " + name + ", Age: " + age);
