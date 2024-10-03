aa414af5ba306b06034dc461d476a4d682f4d50b
Muhammad faris shafwan <73006228+Faris072@users.noreply.github.com>
2024-10-03 18:39:58 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,7 +4,7 @@ class Person {
     public String name;
     private int age;
 
-    public Person(name, age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -15,7 +15,7 @@ class Person {
         fungsi displayInfo tidak pernah di gunakan baik di class main ataupun selain main.
         method displayInfo ini hanya untuk override pada class turunannya saja.
         pembentukan object person sendiri tidak pernah dilakukan di main, displayInfo hanya untuk override. 
-        seharusnya dihapus saja
+        seharusnya tidak perlu dilakukan
     */
     public void displayInfo() {
         System.out.println("Name: " + name + ", Age: " + age);
