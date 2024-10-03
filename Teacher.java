aae3ebeab8fd84f7d2e4dfa892c34b0a18f591e5
Muhammad faris shafwan <73006228+Faris072@users.noreply.github.com>
2024-10-03 18:03:57 +0700

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 class Teacher extends Person {
     private String subject;
      // Melanggar Konsep Clean Code SRP (Single Responsibility), karena class Teacher menangani input, seharusnya hanya attribute dan method
-    public Teacher() {
-        super();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Masukkan mata pelajaran: ");
-        this.subject = scanner.nextLine();
+    public Teacher(name, age, subject) {
+        super(name, age);
+        this.subject =  subject;
     }
 
     @Override
