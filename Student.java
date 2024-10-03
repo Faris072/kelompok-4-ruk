cbddc4f6a8b49461836dd834b950cd608e62e2d0
Muhammad faris shafwan <73006228+Faris072@users.noreply.github.com>
2024-10-03 18:39:30 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Student extends Person {
     private String major;
 
-    public Student(name, age, major) {
+    public Student(String name, int age, String major) {
         super(name, age);
         this.major = scanner.nextLine();
     }
